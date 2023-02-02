d753818b070c27fadc8873d00ce3d5951295af03
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-02-02 12:37:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @GreyMatter_Bots</b>
+<b>© @LusiFilms</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : LusiFilms Filter Bot</b>
 
-⭕️<b>Creater :</b> @GreyMatter_Bots    
+⭕️<b>Creater :</b> @LusiFilms    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
