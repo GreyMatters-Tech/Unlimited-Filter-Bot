368e37ea3d35387190bd7b9c1ff7b01e5ce4244e
cobrafilter <144029811+cobrafilter@users.noreply.github.com>
2023-09-20 20:48:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """ Hey, {},
 
 I'm an advanced filter bot with many capabilities!
 There is no practical limits for my filtering capacity :)
